523b7bd51af90cf02b35954d1d97e33ac8f7bb93
Har1se <timuratai734@gmail.com>
2025-04-14 19:03:45 +0500

    update

--- SupportHandler.java
+++ SupportHandler.java
@@ -1,4 +1,25 @@
-public interface SupportHandler {
-    SupportHandler setNext(SupportHandler handler);
-    void handle(String issue);
+import java.util.logging.Logger;
+
+abstract class SupportHandler {
+    private SupportHandler nextHandler;
+    protected Logger logger = Logger.getLogger(this.getClass().getName());
+
+    public SupportHandler setNext(SupportHandler handler) {
+        this.nextHandler = handler;
+        return handler;
+    }
+
+    public void handle(String issue) {
+        if (canHandle(issue)) {
+            process(issue);
+        } else if (nextHandler != null) {
+            logger.info("[" + this.getClass().getSimpleName() + "] Cannot handle " + issue + " - forwarding to next handler");
+            nextHandler.handle(issue);
+        } else {
+            logger.info("[" + this.getClass().getSimpleName() + "] Cannot handle " + issue + " - escalate manually");
+        }
+    }
+
+    protected abstract boolean canHandle(String issue);
+    protected abstract void process(String issue);
 }
